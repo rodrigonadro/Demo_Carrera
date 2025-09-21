933f5ccc76eeb84ca6d96c4c2dd59d86af2ae3fe
De Anda Flores Rolando Rodrigo <Rolando_DeAnda@nadro.com.mx>
2025-09-20 18:37:00 -0600

    Cambios para subir en github

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'cert/key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'cert/cert.pem')),
-    },
-    port: 5173,
-    // Agregamos todos los hosts que quieres usar
-    host: ['localhost', 'vite.farmatodo.com.mx', 'farmatodo.com.mx', 'carrera.farmatodo.com.mx'],
-  },
+  base: '/Demo_Carrera/'  // ruta base para Github Pages
 })
