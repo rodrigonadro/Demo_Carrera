741030799bfb4bf35a8190b1f90f2b6b5c86814a
De Anda Flores Rolando Rodrigo <Rolando_DeAnda@nadro.com.mx>
2025-09-20 19:18:22 -0600

    Ajuste minúsculas

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Demo_Carrera/'  // ruta base para Github Pages
+  base: '/demo_carrera/'  // ruta base para Github Pages
 })
