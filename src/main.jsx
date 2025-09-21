08bb7e1f2c3d8fb8e91cdbd71f585d6a22afbc07
De Anda Flores Rolando Rodrigo <Rolando_DeAnda@nadro.com.mx>
2025-09-20 23:31:12 -0600

    Ajustes para que se vea los links

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* 👇 Ajuste clave para GitHub Pages */}
-    <BrowserRouter basename="/Demo_Carrera">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
+
