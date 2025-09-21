ff2c10a261b5d83cc45fe2a78affc7a89fa0c500
De Anda Flores Rolando Rodrigo <Rolando_DeAnda@nadro.com.mx>
2025-09-20 22:52:33 -0600

    Ajustes para publicar

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter basename="/Demo_Carrera">
+    <App />
+  </BrowserRouter>
 )
