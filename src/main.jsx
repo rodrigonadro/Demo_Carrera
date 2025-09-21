8217dfe7db3ad0511af345c385eee97e6094b938
De Anda Flores Rolando Rodrigo <Rolando_DeAnda@nadro.com.mx>
2025-09-20 23:09:22 -0600

    ajustes git

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
+import App from './App'
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* 👇 Ajuste clave para GitHub Pages */}
+    <BrowserRouter basename="/Demo_Carrera">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
