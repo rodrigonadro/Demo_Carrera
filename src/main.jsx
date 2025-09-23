96499465123a2f7ed4ac3fbcb37ba50eafeafd6c
De Anda Flores Rolando Rodrigo <Rolando_DeAnda@nadro.com.mx>
2025-09-23 06:35:18 -0600

    Se ajustan para mostrar correctamente rutas

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
-import App from './App'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* 👇 HashRouter evita los 404 en GitHub Pages */}
-    <HashRouter>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
       <App />
-    </HashRouter>
-  </React.StrictMode>
+    </BrowserRouter>
+  </StrictMode>,
 )
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
 const isProduction = process.env.NODE_ENV === 'production'
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'cert/key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'cert/cert.pem')),
+    },
+    port: 5173,
+    // Agregamos todos los hosts que quieres usar
+    host: ['localhost', 'vite.farmatodo.com.mx', 'farmatodo.com.mx', 'carreracdmx.farmatodo.com.mx'],
+  },
   base: isProduction ? '/Demo_Carrera/' : '/',
 })
