81e9cf21e5d386c7ccf708bc4677d80e4634652f
rodrigonadro <138949133+rodrigonadro@users.noreply.github.com>
2025-10-02 22:19:00 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Demo_Carrera">
       <App />
     </BrowserRouter>
   </StrictMode>,
