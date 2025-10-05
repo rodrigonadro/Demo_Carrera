51d41ab9107212643e51bf1c9e866365b901dc01
De Anda Flores Rolando Rodrigo <Rolando_DeAnda@nadro.com.mx>
2025-10-04 21:49:02 -0600

    AJustes para ver en desarrollo y github

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
+const base = import.meta.env.MODE === 'production' ? '/Demo_Carrera' : '/'
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter basename="/Demo_Carrera">
+  <React.StrictMode>
+    <BrowserRouter basename={base}>
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </React.StrictMode>
 )
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs'
-import path from 'path'
-const isProduction = process.env.NODE_ENV === 'production'
+
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'cert/key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'cert/cert.pem')),
-    },
-    port: 5173,
-    // Agregamos todos los hosts que quieres usar
-    host: ['localhost', 'carreracdmx.farmatodo.com.mx'],
-  },
-   // 👇 base dinámica según entorno
-  base: isProduction ? '/Demo_Carrera/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Demo_Carrera/' : '/', // ✅
 })
